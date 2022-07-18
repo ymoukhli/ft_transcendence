2b50b9faa30740540fe63903d6b08c9b3492e7b9
ymoukhli <ymoukhli.1337@gmail.com>
2022-07-18 21:35:42 +0000

    ..

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, HStack, Spacer } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -5,18 +6,19 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <side></side>
-      <main>
+    <HStack>
+      <Box bg='#051886' w='30vw' h='100vh'>
+      </Box>
+      <Box w='70vw'>
         {/* login / register */}
-        <div>
+        <Flex direction='column' h='100vh' justify={'center'} align='center'>
           {/* modal */}
-          <div></div>
+          <Box h='524px' w='486px' bg='#3855AA' borderRadius='2em'></Box>
           {/* footer message */}
-          <div></div>
-        </div>
-      </main>
-    </>
+          <Box position={'sticky'} top='95vh'>don’t have an account yet ? SIGN UP</Box>
+        </Flex>
+      </Box>
+    </HStack>
   );
 }
 
