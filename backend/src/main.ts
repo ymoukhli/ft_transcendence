d3ab6f08973bfac7c47b684bc5a0485160c75088
achraf faddoul <faddoulachraf39@gmail.com>
2022-10-03 22:22:47 +0100

    move main module to main dir

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './main/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
