7d952cb3cc7642297ca5f0e61cee45f074132455
msidqi <mouadmr89@gmail.com>
2022-10-18 21:52:15 +0100

    feat: refactor controls

--- client/features/game/modules/Game/index.tsx
+++ client/features/game/modules/Game/index.tsx
@@ -1,12 +1,14 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Mesh } from 'three';
 import { Box } from '@chakra-ui/react';
-import { useRef } from 'react';
+import React, { createRef, RefAttributes, useRef } from 'react';
 import Paddle from 'features/game/components/Paddle';
 import useDevControls from 'utils/useDevControls';
-import { OrthographicCamera } from '@react-three/drei';
+import { OrthographicCamera, PerspectiveCamera } from '@react-three/drei';
 import { Physics, useSphere } from '@react-three/cannon';
 import ControllerWrapper from 'features/game/components/ControllerWrapper';
+import useMeasure from 'react-use-measure';
+import { useLocalPlayerControls } from 'utils/usePlayerControls';
 
 type GameProps = {
   gameId: string;
@@ -31,18 +33,70 @@ const Cube = () => {
 };
 
 const Game = ({}: GameProps) => {
-  const { zoom, position } = useDevControls({ position: [0, 0, 3], zoom: 40 });
-
+  const {
+    zoom,
+    position,
+    fov,
+    initialPlayerPaddlePosition,
+    initialOpponentPaddlePosition,
+    enableFullControl,
+  } = useDevControls({
+    position: [0, 0, 30],
+    zoom: 1,
+    fov: 75,
+    initialPlayerPaddlePosition: [38, 0, 0],
+    initialOpponentPaddlePosition: [-38, 0, 0],
+    enableFullControl: false,
+  });
+  const [ref, bounds] = useMeasure();
+  const { isPressingUp, isPressingDown, isPressingRight, isPressingLeft } =
+    useLocalPlayerControls();
   return (
-    <Box id="#canvas-container" h="full">
-      <Canvas>
-        <OrthographicCamera position={position} zoom={zoom} makeDefault />
+    <Box id="#canvas-container" h={`${(bounds.width * 9) / 16}px`} bg="black">
+      <Canvas
+        ref={ref}
+        dpr={
+          typeof window !== 'undefined' ? window.devicePixelRatio : undefined
+        }
+      >
+        <PerspectiveCamera
+          fov={fov}
+          position={position}
+          zoom={zoom}
+          makeDefault
+          aspect={
+            typeof window !== 'undefined'
+              ? window.innerWidth / window.innerHeight
+              : undefined
+          }
+        />
+        {/* <OrthographicCamera position={position} zoom={zoom} makeDefault /> */}
         <ambientLight color="000000" intensity={1} />
         <directionalLight color="red" position={[1, 1, 5]} />
         <ControllerWrapper>
           <Physics>
             <Cube />
-            <Paddle />
+            <Paddle
+              initialX={initialPlayerPaddlePosition[0]}
+              initialY={initialPlayerPaddlePosition[1]}
+              initialZ={initialPlayerPaddlePosition[2]}
+              enableFullControl={enableFullControl}
+              isPressingUp={isPressingUp}
+              isPressingDown={isPressingDown}
+              isPressingRight={isPressingRight}
+              isPressingLeft={isPressingLeft}
+            />
+
+            <Paddle
+              initialX={initialOpponentPaddlePosition[0]}
+              initialY={initialOpponentPaddlePosition[1]}
+              initialZ={initialOpponentPaddlePosition[2]}
+              enableFullControl={enableFullControl}
+              isPressingUp={false}
+              isPressingDown={false}
+              isPressingRight={false}
+              isPressingLeft={false}
+            />
           </Physics>
         </ControllerWrapper>
       </Canvas>
