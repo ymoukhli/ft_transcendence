6e6fab7ac28a592abe4d69157a57ed9338daf0a2
achraf faddoul <faddoulachraf39@gmail.com>
2022-10-29 23:57:16 +0100

    [TRAN-3] Add session serializer class

---
+++ backend/src/modules/auth/utils/serializer.ts
@@ -0,0 +1,23 @@
+import { PassportSerializer } from '@nestjs/passport';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from 'src/modules/users/entities/user.entity';
+import { Done } from './types';
+import { AuthenticationProvider } from '../auth';
+
+@Injectable()
+export class SessionSerializer extends PassportSerializer {
+  constructor(
+    @Inject('AUTH_SERVICE')
+    private readonly authService: AuthenticationProvider,
+  ) {
+    super();
+  }
+  serializeUser(user: User, done: Done) {
+    done(null, user);
+  }
+
+  async deserializeUser(user: User, done: Done) {
+    const userDB = await this.authService.findUser(user.login);
+    return userDB ? done(null, userDB) : done(null, null);
+  }
+}
