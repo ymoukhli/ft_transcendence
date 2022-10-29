3574c7f61929769fa7821165aa9850b5e9be4f7b
achraf faddoul <faddoulachraf39@gmail.com>
2022-10-29 23:52:20 +0100

    [TRAN-3] Setup main - add enable cors - add global validation pipe - setup redis - setup session config - initialize passport and setup session

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './main/app.module';
+import { AppModule } from './modules/main/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as redis from 'redis';
+import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
+import * as passport from 'passport';
+import { ONE_DAY_IN_MS } from './modules/constants';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +21,33 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  const client = redis.createClient({
+    url: process.env.REDIS_URI,
+    legacyMode: true,
+  });
+  const RedisStore = connectRedis(session);
+  client.connect();
+  app.use(
+    session({
+      cookie: {
+        maxAge: ONE_DAY_IN_MS,
+        secure: process.env.NODE_ENV === 'production',
+      },
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: new RedisStore({ client }),
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(4000);
 }
 bootstrap();
