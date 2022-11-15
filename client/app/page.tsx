f1c6566d8adc05d477f1b68c734e331a5e1f3606
ymoukhli <ymoukhli.1337@gmail.com>
2022-11-15 22:52:03 +0000

    working on placeholder home page ...

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,18 @@
+'use client';
 import type { NextPage } from 'next';
 import Layout from 'ui/Layout';
-
+import { Flex, Box, Center, Link } from '@chakra-ui/react';
+import NextLink from 'next/link'
 const Home: NextPage = () => {
-  return <Layout>Home page</Layout>;
+  return (<Layout>
+    <Center h="100vh" w="100vw">
+      <Flex align={"center"} justify={"center"} borderRadius='md' bg='tomato' color='white' w="600px" maxW={"900px"} h="450px">
+      <NextLink href='/auth' legacyBehavior passHref>
+        <Link  as="button" px={"30px"} py={"3px"} bg="chocolate">login</Link>
+      </NextLink>
+      </Flex>
+    </Center>
+  </Layout>);
 };
 
 export default Home;
